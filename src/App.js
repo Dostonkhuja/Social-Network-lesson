e0c81f1c2bb3232754c2f0ecc076b9d434d2d76d
Dostonkhuja <sheraliyevdostonhoja@gamil.com>
2021-01-18 02:12:40 +0300

    added Dialogs.jsx

--- src/App.js
+++ src/App.js
@@ -3,13 +3,17 @@ import './App.css';
 import Header from "./Components/Header/Header";
 import Nav from "./Components/Nav/Nav";
 import Profile from "./Components/Profile/Profile";
+import Dialogs from "./Components/Dialogs/Dialogs";
 
 function App() {
     return (
         <div className='app-wrapper'>
             <Header />
             <Nav />
-            <Profile />
+            <div className="app-wrapper-content">
+                <Dialogs />
+                {/*<Profile />*/}
+            </div>
         </div>
     );
 }
