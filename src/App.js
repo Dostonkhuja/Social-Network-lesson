48182067c2d54e80896681c70f7bb0795bfaabfe
Dostonkhuja <sheraliyevdostonhoja@gamil.com>
2021-01-18 03:44:57 +0300

    added react-router-dom

--- src/App.js
+++ src/App.js
@@ -4,17 +4,24 @@ import Header from "./Components/Header/Header";
 import Nav from "./Components/Nav/Nav";
 import Profile from "./Components/Profile/Profile";
 import Dialogs from "./Components/Dialogs/Dialogs";
+import  {Route,BrowserRouter} from "react-router-dom";
 
 function App() {
     return (
-        <div className='app-wrapper'>
-            <Header />
-            <Nav />
-            <div className="app-wrapper-content">
-                <Dialogs />
-                {/*<Profile />*/}
+        <BrowserRouter>
+            <div className='app-wrapper'>
+                <Header/>
+                <Nav/>
+                <div className="app-wrapper-content">
+                    <Route path='/dialogs' component={Dialogs}/>
+                    <Route path='/profile' component={Profile}/>
+                    {/*<Route path='/messages' component={Messages}/>*/}
+                    {/*<Route path='/news' component={News}/>*/}
+                    {/*<Route path='/music' component={Music}/>*/}
+                    {/*<Route path='/settigns' component={Settings}/>*/}
+                </div>
             </div>
-        </div>
+        </BrowserRouter>
     );
 }
 
