0a5e17887d4925281e879a5415b57d6dd7f4846f
Dostonkhuja <sheraliyevdostonhoja@gamil.com>
2021-01-27 05:06:10 +0300

    Users.jsx changed to class

--- src/Components/Nav/Nav.jsx
+++ src/Components/Nav/Nav.jsx
@@ -10,10 +10,10 @@ const Nav = (props) => {
                 <NavLink to='/profile' activeClassName={s.activeLink}> Profile </NavLink>
             </div>
             <div className={s.item}>
-                <NavLink to='/dialogs' activeClassName={s.activeLink}> Dialogs </NavLink>
+                <NavLink to='/dialogs' activeClassName={s.activeLink}> Messages </NavLink>
             </div>
             <div className={s.item}>
-                <NavLink to='/messages' activeClassName={s.activeLink}> Messages </NavLink>
+                <NavLink to='/users' activeClassName={s.activeLink}> Users </NavLink>
             </div>
             <div className={s.item}>
                 <NavLink to='/news' activeClassName={s.activeLink}> News </NavLink>
