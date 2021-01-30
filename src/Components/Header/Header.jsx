ca8af67ecc0458b8a40a9631784147090f79d36c
Dostonkhuja <sheraliyevdostonhoja@gamil.com>
2021-01-30 17:01:16 +0300

    added auth-reducer.js (login auth/me)

--- src/Components/Header/Header.jsx
+++ src/Components/Header/Header.jsx
@@ -1,10 +1,14 @@
 import React from "react";
 import s from './header.module.css'
+import {NavLink} from "react-router-dom";
 
-const Header = () => {
+const Header = (props) => {
     return (
         <header className={s.header}>
-            <img src="http://pluspng.com/img-png/the-beatles-png-the-beatles-by-andreza0406-the-beatles-by-andreza0406-900.png"/>
+                <img src="http://pluspng.com/img-png/the-beatles-png-the-beatles-by-andreza0406-the-beatles-by-andreza0406-900.png"/>
+                <div className={s.loginBlock}>
+                    {props.isAuth ? props.login:<NavLink to={'/login'}>Login</NavLink>}
+                </div>
         </header>
     )
 }
