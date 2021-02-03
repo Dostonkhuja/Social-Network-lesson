0a9feb910138cc3b6098690760068bcd23ec69fc
Dostonkhuja <sheraliyevdostonhoja@gamil.com>
2021-02-03 12:24:50 +0300

    added localState for ProfileStatus.jsx

--- src/Components/Profile/ProfileInfo/Profile.jsx
+++ src/Components/Profile/ProfileInfo/Profile.jsx
@@ -1,21 +1,23 @@
 import React from "react";
 import s from './profileInfo.module.css'
 import Preloader from "../../common/preloader/Preloader";
+import ProfileStatus from "./ProfileStatus";
 
 const ProfileInfo = (props) => {
     if (!props.profile) {return <Preloader />}
 
     return (
         <div>
-            <div>
-                <img className={s.contentImg}
-                     src="http://getwallpapers.com/wallpaper/full/7/0/9/1340311-abbey-road-wallpaper-3440x1440-for-macbook.jpg"/>
-            </div>
+            {/*<div>*/}
+            {/*    <img className={s.contentImg}*/}
+            {/*         src="http://getwallpapers.com/wallpaper/full/7/0/9/1340311-abbey-road-wallpaper-3440x1440-for-macbook.jpg"/>*/}
+            {/*</div>*/}
             <div className={s.descriptionBlock}>
                 <div>
                     <img src={props.profile.photos.large} />
                     <h4>{props.profile.fullName}</h4>
-                    <h3><i>{props.profile.aboutMe}</i></h3>
+                    <ProfileStatus status={'hello my friends'}/>
+                    {/*<h3><i>{props.profile.aboutMe}</i></h3>*/}
                     <a href={props.profile.github}></a>
                     <span>{props.profile.lookingForAJob ? <img src="http://www.animatedgif.net/sitemessages/search/w-search_ani_e0.gif"/> :''}</span>
                     <p>{props.profile.lookingForAJobDescription}</p>
